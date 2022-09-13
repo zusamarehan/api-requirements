13a0521c312654f41d9bba509f9db2993b6a4e8e
Rehan KU <zrehan286@gmail.com>
2022-09-13 19:47:15 +0530

    setup: added models & migration & seeder for the products module

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::insert($this->getJsonData());
+    }
+
+    protected function getJsonData()
+    {
+        $path = database_path() . "/seeders/data.json";
+
+        return json_decode(file_get_contents($path), true)['products'];
+    }
+}
